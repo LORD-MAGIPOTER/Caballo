79a82d12e750d8d0eb6d73a7fb6f4301a3fa047f
David Bautista <davidbaar07@gmail.com>
2024-03-04 13:14:49 -0600

    Cambiar diseño

---
+++ Operaciones/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace Operaciones
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Aceptar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numero1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numero2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
